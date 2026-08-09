d5a1fb1d2bc1058a8fc04030c62c013292ad7b09
xxxily <974278171@qq.com>
2026-08-10 00:08:42 +0800

    chore: release v0.4.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "site.anzz.childkiosk"
         minSdk = 28 // API 28 (Android 9.0 Pie) 是 setLockTaskFeatures 的推荐版本
         targetSdk = 34
-        versionCode = 104
-        versionName = "0.4.29"
+        versionCode = 105
+        versionName = "0.4.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
